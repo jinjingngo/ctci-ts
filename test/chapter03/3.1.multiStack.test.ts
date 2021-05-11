f454a3df737b317ccedf5680a934a27d12372735
wujinjing <wujinjing@kedacom.com>
2021-05-11 13:12:01 +0800

    conquer

--- test/chapter03/3.1.multiStack.test.ts
+++ test/chapter03/3.1.multiStack.test.ts
@@ -7,7 +7,7 @@ for (const Stack of MultiStacks) {
     });
     it('can push and pop value from middle stack correctly', () => {
       const stack: number[] = [];
-      for (let i = 1; i < 100; i += 4) { 
+      for (let i = 1; i < 5; i += 1) {
         const val = Math.trunc(Math.random() * 999999);
         tripleStack.push(2, val);
         stack.push(val);
@@ -17,17 +17,17 @@ for (const Stack of MultiStacks) {
 
     it('can push, peek and pop values from all 3 stack correctly', () => {
       const stacks: Array<number[]> = [[], [], []];
-      for (let j = 9; j > 0; --j) {
-        for (let i = 1; i <= 3; ++i) {
+      for (let j = 5; j > 0; --j) {
+        for (let i = 0; i < 3; ++i) {
           const val: number = i * 10 + j;
           tripleStack.push(i, val);
-          stacks[i - 1].push(val);
+          stacks[i].push(val);
           expect(tripleStack.peek(i)).toBe(val);
         }
       }
 
-      for (let i = 1; i <= 3; ++i) {
-        stacks[i - 1]
+      for (let i = 0; i < 3; ++i) {
+        stacks[i]
           .reverse()
           .forEach(v => expect(tripleStack.pop(i)).toBe(v));
         expect(tripleStack.isEmpty(i)).toBeTruthy();
